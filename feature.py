2eb167a32d6ad76ab6bdfff9cdc01fed05c79cc0
Prashant Shukla <prshukla.edu@gmail.com>
2023-08-26 11:40:09 +0530

    Update feature.py

--- feature.py
+++ feature.py
@@ -3,6 +3,7 @@ from sklearn.decomposition import PCA
 class FeatureSelection:
 
     '''
+    Provides different implementation of dimension reduction
     '''
 
     def __init__(self) -> None:
@@ -13,6 +14,20 @@ class FeatureSelection:
                   num_pca_components=8):
 
         '''
+        Applies PCA technique for dimension reduction
+
+        Parameters
+        ----------
+        X: np.array
+            n-dim input data of spikes
+        
+        num_pca_components: int
+            Desired dimennsion of data
+
+        Return
+        ------
+        self.__pca_model: PCA
+            PCA model to find principal components
         '''
 
         self.__pca_model = PCA(n_components = num_pca_components).fit(X)
@@ -23,6 +38,7 @@ class FeatureSelection:
                      X):
 
         '''
+        Applies fitted PCA model  on spike data
         '''
 
         return self.__pca_model.fit_transform(X)
